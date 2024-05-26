aed3e4a48e88f278a71bc5ab85b9f3e33f40ed33
Jean Philippe Ulpiano <jean.ulpiano@kpit.com>
2024-05-26 15:41:53 +0200

    Modified schema

--- src/routes/five-nights.js
+++ src/routes/five-nights.js
@@ -18,13 +18,13 @@ router.post("/", asyncMiddleware(async (req, res) => {
 
   const untilTime = Math.floor(new Date().getTime() / 1000 / 60) + timeMinutes;
 
-  /*const result = await prisma.AccessTime.create({
+  const result = await prisma.GameAccessTime.create({
     data: {
       until_time: untilTime
     }
-  });*/
-  const deleteUsers = await prisma.AccessTime.deleteMany({})
-  const result = await prisma.user.upsert({
+  });
+  const deleteUsers = await prisma.GameAccessTime.deleteMany({})
+  /*const result = await prisma.user.upsert({
     where: {
     },
     update: {
@@ -33,14 +33,14 @@ router.post("/", asyncMiddleware(async (req, res) => {
       until_time: untilTime,
 
     },
-  })
+  })*/
   res.json(result);
 }));
 
 router.get('/', asyncMiddleware(async (req, res) => {
   try {
     const now = Math.floor(new Date().getTime() / 1000 / 60);
-    const findTime = await prisma.AccessTime.findFirstOrThrow({
+    const findTime = await prisma.GameAccessTime.findFirstOrThrow({
       where: {
         until_time: {
               gt: now,
@@ -59,7 +59,7 @@ router.get('/', asyncMiddleware(async (req, res) => {
 
 /*router.patch('/:id', asyncMiddleware(async (req, res) => {
   const { id } = req.params;
-  const updated = await prisma.AccessTime.update({
+  const updated = await prisma.GameAccessTime.update({
     where: { id },
     data: req.body,
   });
