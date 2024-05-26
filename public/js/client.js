4c13c30a89e7efe4040f03b01f3b3a398b44c368
Jean Philippe Ulpiano <jean.ulpiano@kpit.com>
2024-05-26 17:36:37 +0200

    Test HTML

--- public/js/client.js
+++ public/js/client.js
@@ -18,7 +18,41 @@ const apiFetch = async (url, method, body) => {
 
 const initPlaytime = () => {
 
+    const renderTitle = ({ title }) => {
+        return `<b>${title}</b>`;
+    };
+    const renderItem = ({ id, title, done }) => `
+        <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded" style="background-color: #f4f6f7;">
+            <input
+                class="form-check-input me-2"
+                type="checkbox"
+                value=""
+                ${done ? "checked" : ""}
+                onchange="handleTodoChange(event, '${id}')"
+            />
+            <div style="flex-grow: 1;">
+                ${renderTitle({ title })}
+            </div>
+            <!-- INSERT DELETE BUTTON HERE -->
+        </li>
+    `;
+    const noItems = `
+        <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded" style="background-color: #f4f6f7;">
+            <strong>No tasks yet. Try adding one.</strong>
+        </li>
+    `;
 
+    const refreshList = () => {
+        const doRefresh = async () => {
+            const list = document.querySelector("#todo-list");
+
+            const resp = await apiFetch("/fivenights");
+            const todos = await resp.json();
+            list.innerHTML = todos.map(renderItem).join("");
+
+        };
+
+    /*
     const refreshList = () => {
         const doRefresh = async () => {
             const list = document.querySelector("#fivenights");
@@ -45,7 +79,7 @@ const initPlaytime = () => {
                 //fiveNights.map(renderItem).join("");
             }
         };
-
+        */
         doRefresh().catch(err => console.log("Error refreshing list", err));
     };
 
